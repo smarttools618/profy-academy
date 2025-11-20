c213f83fe7b0df9afc3343a130f45e20f2d1bf59
smarttools <smarttools618@gmail.com>
2025-11-20 22:34:14 +0100

    new website update

--- src/lib/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import type { Database } from '@/types/supabase';
-
-// Server-side Supabase client for use in Server Components and Route Handlers
-export const createClient = () =>
-  createServerComponentClient<Database>({ cookies });
